ec8bfdb6c40ccdc35ed8f6b890f56a04d1f5209b
Lucas Calabria <lucascalabria@ymail.com>
2022-01-12 12:50:10 -0300

    editPage - rota

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import AddPage from "./pages/addPage";
-import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import { AddPage, EditPage }from "./pages";
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route exact path = "/" element={<AddPage />} />
+          <Route exact path = "/add" element={<AddPage />} />
+          <Route exact path = "/edit" element={<EditPage />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,2 @@
+export {default as AddPage} from './addPage'
+export {default as EditPage} from './editPage'
